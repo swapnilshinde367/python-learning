1d341df6e75e1dfd9a75991584401895a802599a
swapnil <swapnil@xchangings-MacBook-Pro.local>
2017-05-05 00:09:22 +0800

    Create class.  Create class variables, class methods.

---
+++ CEmployee.py
@@ -0,0 +1,34 @@
+# pylint: skip-file
+# Create class
+# Create class variables, class methods.
+
+class CEmployee:
+
+	intSalary		= 0
+	strFirstName	= ''
+	strEmailAddress	= ''
+	strLastName		= ''
+
+	def __init__(self):
+		pass
+
+	def handleCreateEmailAddress( self ):
+		self.strEmailAddress = self.strFirstName + '.' + self.strLastName + '@' + 'company.com'
+
+objEmployee1 = CEmployee()
+objEmployee1.strFirstName	= 'John'
+objEmployee1.strLastName	= 'Cena'
+objEmployee1.intSalary		= 1500
+
+objEmployee2 = CEmployee()
+objEmployee2.strFirstName	= 'Randy'
+objEmployee2.strLastName	= 'Orton'
+objEmployee2.intSalary		= 2000
+
+objEmployee1.handleCreateEmailAddress()
+
+objEmployee2.handleCreateEmailAddress()
+
+print( objEmployee1.strEmailAddress )
+
+print( objEmployee2.strEmailAddress )
