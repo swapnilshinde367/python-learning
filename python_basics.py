587671788380eed4477d91e9bf1c7ef5d0594065
swapnil <swapnil@xchangings-MacBook-Pro.local>
2017-06-04 01:30:56 +0800

    line continuation using "\"

---
+++ python_basics.py
@@ -0,0 +1,26 @@
+# pylint: skip-file
+
+# line continuation using "\"
+# quotes to creates strings
+
+test_var = 'abc' + \
+			'-def-' + \
+			'ghi'
+
+print test_var
+
+
+# quotes
+# single quotes for word
+word = 'word'
+print word
+
+# double quotes for sentence
+sentence = "This is a sentence."
+print sentence
+
+# triple quotes can be used for multiline string
+paragraph = """This is a paragraph. It is
+made up of multiple lines and sentences."""
+
+print paragraph
