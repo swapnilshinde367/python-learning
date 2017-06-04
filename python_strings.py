3d6cbb795480abdc30426f4916a00eee5c31c9e4
swapnil <swapnil@xchangings-MacBook-Pro.local>
2017-06-05 00:25:46 +0800

    strings, their operations, concat, repeat, string selctive characters

---
+++ python_strings.py
@@ -0,0 +1,23 @@
+# pylint: skip-file
+
+# strings, their operations, concat, repeat, string selctive characters
+
+strName = 'Swapnil Shinde'
+
+# Prints full string
+print strName
+
+# Prints 4th character
+print strName[4]
+
+# Prints 4th to 10th character
+print strName[4 : 10]
+
+# Prints from 8th to last character
+print strName[8 :]
+
+# Concat "Test" to string
+print strName + ' Test'
+
+# Repeat string 2 times
+print strName * 2
