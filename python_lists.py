7adecf4a4fe4aff1a42615c78a7ee5bd763df18d
swapnil <swapnil@xchangings-MacBook-Pro.local>
2017-06-05 00:53:34 +0800

    lists, their operations, concat, repeat, string selctive elements

---
+++ python_lists.py
@@ -0,0 +1,24 @@
+# pylint: skip-file
+
+# lists, their operations, concat, repeat, string selctive elements
+
+list = [ 'abcd', 345 , 2.23, 'randy', 70.2 ]
+tinylist = [123, 'orton']
+
+# Print whole list
+print list
+
+# Print first element
+print list[1]
+
+# Print first to fourth element
+print list[1 : 4]
+
+# Print second to last element
+print list[2 :]
+
+# Concat the 2 lists
+print list + tinylist
+
+# Print the list 2 times
+print tinylist * 2
