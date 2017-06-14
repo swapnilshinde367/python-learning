2f983ccb9508b091a1124faaba7d7aa8fd92b573
swapnil <swapnil@xchangings-MacBook-Pro.local>
2017-06-15 00:45:06 +0800

    Operators, addition, subtraction, multiplication, division, modules, exponent, Floor division.

---
+++ basic_operators.py
@@ -0,0 +1,24 @@
+# pylint: skip-file
+
+# Operators, addition, subtraction, multiplication, division, modules, exponent, Floor division.
+
+print 10 + 20
+
+print 20 - 5
+
+print 3 * 8
+
+print 14 / 2
+
+print 19 % 6
+
+# Floor Division - The division of operands where the result is the quotient in which the digits after the decimal point are removed.
+# But if one of the operands is negative, the result is floored.
+
+print 11 // 4
+
+print 9 // 2
+
+print -11 // 3
+
+print 11 // -3
