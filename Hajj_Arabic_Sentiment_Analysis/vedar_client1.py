6ea0cffd07a97acd65dd047111679f35b0d555e3
Swapnil Shinde <swapnil.shinde367@gmail.com>
2017-07-12 23:38:11 +0800

    Twitter Sentiment analysis for Hajj

--- Hajj_Arabic_Sentiment_Analysis/vedar_client1.py
+++ Hajj_Arabic_Sentiment_Analysis/vedar_client1.py
@@ -1,28 +1,21 @@
 # pylint: skip-file
-import csv
-import json
-import urllib
-import requests
 import pandas as pandas
+from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-strEndPointUrl	= 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170711T175802Z.3c6b4a88016ebad0.cdbc34c633022687c3a9946cc9b670a542f6ccdf&lang=ar-en'
 strFileName		= raw_input( 'Make sure file is in current directory and enter name of the file.\n' )
 arrExcelData	= pandas.read_excel( './' + strFileName )
 objAnalyzer		= SentimentIntensityAnalyzer()
+to_lang="en"
 
 for strTweet in arrExcelData['text'] :
 	strTweet = strTweet.replace( '\n', ' ' )
 	strTweet = strTweet.replace( ',', ' ' )
-
-	arrmixParameters = {
-		'text' : strTweet
-	}
-
-	objRequest = requests.post( url = strEndPointUrl, params = arrmixParameters )
-	arrmixResponse = objRequest.json()
-
-	strTweet = arrmixResponse['text'][0]
+	b = TextBlob(strTweet)
+	from_lang=b.detect_language()
+	if(from_lang=='ar'):
+		tt=TextBlob(strTweet)
+		strTweet=tt.translate(to="en")
+		strTweet =  str(strTweet)
 	vs = objAnalyzer.polarity_scores(strTweet)
-
 	print str(vs)
