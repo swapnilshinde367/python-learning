ca4af32eb2e6b8809343d19170e4eaf1c2363c50
Swapnil Shinde <swapnil.shinde367@gmail.com>
2017-07-13 00:09:29 +0800

    Twitter Sentiment analysis for Hajj

--- Hajj_Arabic_Sentiment_Analysis/vedar_client1.py
+++ Hajj_Arabic_Sentiment_Analysis/vedar_client1.py
@@ -1,21 +1,31 @@
 # pylint: skip-file
+import sys
+import os.path
 import pandas as pandas
 from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-strFileName		= raw_input( 'Make sure file is in current directory and enter name of the file.\n' )
-arrExcelData	= pandas.read_excel( './' + strFileName )
+strToLanguage	= 'en'
 objAnalyzer		= SentimentIntensityAnalyzer()
-to_lang="en"
+
+strFileName		= raw_input( 'Make sure file is in current directory and enter name of the file.\n' )
+
+if( False == os.path.isfile( './' + strFileName ) ) :
+	print 'No such file'
+	sys.exit()
+
+arrExcelData	= pandas.read_excel( './' + strFileName )
+
 
 for strTweet in arrExcelData['text'] :
 	strTweet = strTweet.replace( '\n', ' ' )
 	strTweet = strTweet.replace( ',', ' ' )
-	b = TextBlob(strTweet)
-	from_lang=b.detect_language()
-	if(from_lang=='ar'):
-		tt=TextBlob(strTweet)
-		strTweet=tt.translate(to="en")
-		strTweet =  str(strTweet)
-	vs = objAnalyzer.polarity_scores(strTweet)
-	print str(vs)
+	objTextBlob = TextBlob( strTweet )
+	strFromLanguage = objTextBlob.detect_language()
+	if( 'ar' == strFromLanguage ):
+		strTweet = objTextBlob.translate( to = 'en' )
+		strTweet = str( strTweet )
+	objVaderAnalysis = objAnalyzer.polarity_scores(strTweet)
+	# print 'Analysis of ' + strTweet + ' is \n'
+	print strTweet
+	print str( objVaderAnalysis )
