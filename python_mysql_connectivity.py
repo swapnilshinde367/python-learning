ae58c24cfcc93c2c8d9291635d76905383493be6
Swapnil Shinde <swapnil.shinde367@gmail.com>
2017-07-30 22:23:04 +0800

    Mysql Connectivity

---
+++ python_mysql_connectivity.py
@@ -0,0 +1,24 @@
+# pylint: skip-file
+import mysql.connector
+
+objMysqlConnection	= mysql.connector.connect(	host		= 'localhost',
+												database	= 'db_name',
+												user		= 'root',
+												password	= 'password' )
+
+
+
+strQuery = 'INSERT INTO `test_table` \
+(`user_name`, \
+`original_tweet`, \
+`translated_tweet`, \
+`score`, \
+`positive`, \
+`negative`, \
+`neutral`) \
+VALUES \
+(%s,%s,%s,%s,%s,%s,%s)'
+
+objCursor = objMysqlConnection.cursor()
+objCursor.executemany( strQuery, arrmixDataToInsert )
+objMysqlConnection.commit()
