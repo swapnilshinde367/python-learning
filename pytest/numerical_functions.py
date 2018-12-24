aa8f0f2e5d155eb1e846113c4bf5e0c67378a127
XCHANGING.MY\Swapnil.Shinde <swapnil.shinde@asia.xchanging.com>
2018-12-24 18:10:42 +0800

    skip and marker decorators

--- pytest/numerical_functions.py
+++ pytest/numerical_functions.py
@@ -3,3 +3,9 @@ def addition( intA, intB ) :
 
 def multiplication( intA, intB ) :
 	return intA * intB
+
+def dividebytwo( intA ) :
+	return intA / 2
+
+def dividebyfive( intA ) :
+	return intA / 5
--- pytest/test_numerical_functions.py
+++ pytest/test_numerical_functions.py
@@ -1,12 +1,32 @@
-from numerical_functions import *
+import pytest
+import sys
+import numerical_functions
 
-def test_addition():
-	assert 5 == addition( 2 , 3 )
+# skip if function to skip test based on if condition
+# @pytest.mark.skipif( sys.version_info < ( 3, 5 ), reason = 'Testing Python version' )
+def test_addition() :
+	assert 5 == numerical_functions.addition( 2 , 3 )
 
-def test_multiplication():
-	assert 7 == multiplication( 2 , 3 )
+# skip test function
+# @pytest.mark.skip( reason = 'Skipping Test' )
+def test_multiplication() :
+	assert 6 == numerical_functions.multiplication( 2 , 3 )
 
-# run
+
+# Creating a custom tag so we can run specific tests using that marker
+@pytest.mark.two
+def test_dividebytwo() :
+	assert 2 == numerical_functions.dividebytwo( 4 )
+
+@pytest.mark.five
+def test_dividebyfive() :
+	assert 2 == numerical_functions.dividebyfive( 4 )
+
+# to run use following commands
 # python -m pytest
 # py.test
 # py.test -v
+# py.test -vrxs -> show reason of skipping test
+# pytest -k mult -v -> to run the tests containing mult as substring in their name
+# pytest -m two -v -> run only test which has marker two
+# pytest -m "not two" -v -> run only test which does not have marker two
